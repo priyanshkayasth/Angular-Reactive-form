33bdf40f00a3dde3d763936236ea4c7e5c8de1eb
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-03-27 17:43:07 +0530

    first commit

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  form = new FormGroup({
+
+    email: new FormControl('',{
+      validators:[Validators.email,Validators.required]
+    }),
+    password: new FormControl('',{
+      validators:[Validators.required,Validators.minLength(6)]
+    })
+  })
+
+  get emailIsInValid(){
+    return this.form.controls.email.touched && this.form.controls.email.dirty && this.form.controls.email.invalid
+  }
+
+  get passwordIsInValid(){
+    return this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid
+  }
+
+  onSubmit() {
+    console.log(this.form);
+    const entredEmail = this.form.value.email
+    const enteredPassword = this.form.value.password
+    console.log(entredEmail,enteredPassword);
+    
+
+
+  }
+}
