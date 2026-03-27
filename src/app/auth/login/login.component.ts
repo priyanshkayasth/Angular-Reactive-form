a1b003c3dbc4d2e67460c9722584d36523a685a3
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-03-27 18:25:39 +0530

    save in localstorage

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
-import { of } from 'rxjs';
+import { debounceTime, of } from 'rxjs';
 
 
 function mustControlQuestionaMark(control: AbstractControl) {
@@ -17,6 +17,14 @@ function emailIsUnique(control: AbstractControl) {
   return of({ notUnique: true })
 }
 
+let initialEmailValue=''
+const savedForm = window.localStorage.getItem('saved-login-form')
+if(savedForm){
+  const loadedForm=JSON.parse(savedForm)
+  initialEmailValue=loadedForm.email
+}
+
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -24,10 +32,12 @@ function emailIsUnique(control: AbstractControl) {
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private destroyRef = inject(DestroyRef)
   form = new FormGroup({
 
-    email: new FormControl('', {
+    // email: new FormControl('', {
+    email: new FormControl(initialEmailValue, {
       validators: [Validators.email, Validators.required],
       asyncValidators: [emailIsUnique]
     }),
@@ -42,6 +52,26 @@ export class LoginComponent {
 
   get passwordIsInValid() {
     return this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid
+  }
+
+  ngOnInit() {
+    // const savedForm = window.localStorage.getItem('saved-login-form')
+    // if (savedForm) {
+    //   const loadedForm = JSON.parse(savedForm)
+    //   // this.form.controls.email.setValue
+    //   this.form.patchValue({
+    //     email: loadedForm.email,
+
+    //   })
+    // }
+
+
+    const subacription = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: (value) => {
+        window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email }))
+      }
+    })
+    this.destroyRef.onDestroy(() => subacription.unsubscribe())
   }
 
   onSubmit() {
